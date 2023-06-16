2da068b4813228c200a4bdb6dbc039013e43e72f
Jason Smith <smija13@gmail.com>
2023-06-15 21:55:00 -0700

    Update the information in the career component

--- src/components/Career.js
+++ src/components/Career.js
@@ -3,12 +3,8 @@ export default function Career() {
 		<div className="container" id="career">
 			<header className="career-header">Career</header>
 			<div>
-				<p className=""><span className="job-title">React Apprentice | AlphaWorks |</span> Sep 2021 - Current</p>
-				<p>Job description</p>
-			</div>
-			<div>
-				<p className=""><span className="job-title">Co-Teacher, JavaScript For Beginners | AlphaWorks |</span> Feb 2022 - March 2022</p>
-				<p>Job description</p>
+				<p className=""><span className="job-title">React Apprentice | AlphaWorks |</span> Sep 2021 - March 2023</p>
+				<p>As a React Native apprentice, I worked on client projects as part of a pod, while also learning and developing my skills in the field. I assisted with teaching students and gained valuable experience in React Native development, collaboration, and project success while using agile methodologies.</p>
 			</div>
 		</div>
 	);
