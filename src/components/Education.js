db0b140dfb676799517eac516e0af2d958c224f3
Jason Smith <smija13@gmail.com>
2024-09-25 09:38:26 -0700

    Update education section information

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,8 +3,10 @@ export default function Education() {
         <div className="container" id="education">
             <header className="">Education</header>
             <div>
+                <p className=""><span className="training">Boot.dev backend developer program |</span> July 2024 - Current</p>
+                <p>Training covered Python, OOP, Functional Programming, JavaScript</p>
                 <p className=""><span className="training">BitWise Industries - Workforce Training |</span> Aug - Sep 2021</p>
-                <p>Training covered JavaScript</p>
+                <p>Training covered mobile development with React Native</p>
                 <p className=""><span className="training">Clovis Community College |</span> July 2020</p>
                 <p>Associates of Art in Two Dimensional Art</p>
             </div>
