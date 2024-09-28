edf833b955f98b5c50f60f482044f3ab899e2b47
Jason Smith <smija13@gmail.com>
2024-09-27 23:55:47 -0700

    update career section

--- src/components/Career.js
+++ src/components/Career.js
@@ -3,6 +3,10 @@ export default function Career() {
 		<div className="container" id="career">
 			<header className="career-header">Career</header>
 			<div>
+				<p className=""><span className="job-title">Cart Associate | Walmart |</span> March 2024 - Current</p>
+				<p>As a cart associate, I work with a small team. The primary goal is to gather the shopping carts from the parking lot and bring them into the store. It's important to make sure the carts are clean. I also assist customers with carrying items out or carring item in.</p>
+			</div>
+			<div>
 				<p className=""><span className="job-title">React Apprentice | AlphaWorks |</span> Sep 2021 - March 2023</p>
 				<p>As a React Native apprentice, I worked on client projects as part of a pod, while also learning and developing my skills in the field. I assisted with teaching students and gained valuable experience in React Native development, collaboration, and project success while using agile methodologies.</p>
 			</div>
